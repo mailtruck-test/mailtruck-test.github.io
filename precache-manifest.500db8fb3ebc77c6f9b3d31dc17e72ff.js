07d835ca2716d29a1320d4e8132146682ff7de6f
Penelope Styles <pstyles@mercursa.com>
2020-02-26 09:21:55 -0500

    update build

---
+++ precache-manifest.500db8fb3ebc77c6f9b3d31dc17e72ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54dfbecbb4f16a5b97f8439bc0bf0b20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e396389c453d30272ac7",
+    "url": "/static/css/main.44deff5f.chunk.css"
+  },
+  {
+    "revision": "530fbc615664c55335ec",
+    "url": "/static/js/2.677886d3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.677886d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e396389c453d30272ac7",
+    "url": "/static/js/main.44d9969d.chunk.js"
+  },
+  {
+    "revision": "278b5bbc3250345949db",
+    "url": "/static/js/runtime-main.34e4c8b8.js"
+  }
+]);
